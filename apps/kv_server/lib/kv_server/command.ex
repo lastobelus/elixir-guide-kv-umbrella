1461a79e8d7a022d8cdf6ac633f4d532f78539b7
Michael Johnston <lastobelus@mac.com>
2020-11-10 09:51:37 -0800

    refactor: KVServer.Command.run takes registry as arg,
    unit tests for run for :create, :get, :put, :delete commands

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -48,42 +48,46 @@ defmodule KVServer.Command do
     end
   end
 
-  @spec run(command()) :: response()
+  @spec run(command(), pid()) :: response()
   @doc """
     Runs the given `command`.
+
+    ## Examples
+
+    iex>
   """
   # a bodiless function is used to declare and/or document the
   # default arguments for a multi-clause function
-  def run(command)
+  def run(command, registry)
 
-  def run({:create, bucket}) do
-    KV.Registry.create(KV.Registry, bucket)
+  def run({:create, bucket}, registry) do
+    KV.Registry.create(registry, bucket)
     {:ok, "OK\r\n"}
   end
 
-  def run({:get, bucket, key}) do
-    lookup(bucket, fn pid ->
+  def run({:get, bucket, key}, registry) do
+    lookup(bucket, registry, fn pid ->
       value = KV.Bucket.get(pid, key)
       {:ok, "#{value}\r\nOK\r\n"}
     end)
   end
 
-  def run({:put, bucket, key, value}) do
-    lookup(bucket, fn pid ->
+  def run({:put, bucket, key, value}, registry) do
+    lookup(bucket, registry, fn pid ->
       KV.Bucket.put(pid, key, value)
       {:ok, "OK\r\n"}
     end)
   end
 
-  def run({:delete, bucket, key}) do
-    lookup(bucket, fn pid ->
+  def run({:delete, bucket, key}, registry) do
+    lookup(bucket, registry, fn pid ->
       KV.Bucket.delete(pid, key)
       {:ok, "OK\r\n"}
     end)
   end
 
-  defp lookup(bucket, callback) do
-    case KV.Registry.lookup(KV.Registry, bucket) do
+  defp lookup(bucket, registry, callback) do
+    case KV.Registry.lookup(registry, bucket) do
       {:ok, pid} -> callback.(pid)
       :error -> {:error, :not_found}
     end
--- apps/kv_server/test/kv_server/command_test.exs
+++ apps/kv_server/test/kv_server/command_test.exs
@@ -4,4 +4,40 @@ defmodule KVServer.CommandTest do
   """
   use ExUnit.Case, async: true
   doctest KVServer.Command
+
+  setup context do
+    # start_supervised! is an EXUnit helper that guarantees the process
+    # will be properly stopped & started between test runs
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
+  end
+
+  test "creates a bucket", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:create, "shopping"}, registry)
+    assert {:ok, _bucket} = KV.Registry.lookup(registry, "shopping")
+  end
+
+  test "gets a key ", %{registry: registry} do
+    bucket = KV.Registry.create(registry, "shopping")
+    KV.Bucket.put(bucket, "milk", "7")
+
+    assert {:ok, "7\r\nOK\r\n"} = KVServer.Command.run({:get, "shopping", "milk"}, registry)
+  end
+
+  test "puts a key ", %{registry: registry} do
+    bucket = KV.Registry.create(registry, "shopping")
+
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:put, "shopping", "milk", 8}, registry)
+    assert KV.Bucket.get(bucket, "milk") == 8
+  end
+
+  test "deletes a key", %{registry: registry} do
+    bucket = KV.Registry.create(registry, "shopping")
+    KV.Bucket.put(bucket, "milk", "9")
+
+    assert {:ok, "OK\r\n"} = KVServer.Command.run({:delete, "shopping", "milk"}, registry)
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
